e6fb1d5655d3c86bb995cf9d48a800626dee2838
Fernando Aguilar <davidfer.aguilar02@gmail.com>
2024-09-01 13:34:38 -0500

    add use hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   importProvidersFrom,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { provideRouter, withHashLocation, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,7 +13,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes, withViewTransitions({ skipInitialTransition: true })),
+    provideRouter(routes, withHashLocation(), withViewTransitions({ skipInitialTransition: true })),
 
     importProvidersFrom(BrowserModule),
     importProvidersFrom(BrowserAnimationsModule), provideAnimationsAsync(),
